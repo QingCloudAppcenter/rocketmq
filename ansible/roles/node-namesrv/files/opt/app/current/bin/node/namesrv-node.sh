972c57d28697ed7963e3ed77ef9751578598c1fb
shengpengwang <shengpengwang@yunify.com>
2020-09-03 10:58:31 +0800

    fix custom tab by export JAVA_HOME & update img 2020090310

--- ansible/roles/node-namesrv/files/opt/app/current/bin/node/namesrv-node.sh
+++ ansible/roles/node-namesrv/files/opt/app/current/bin/node/namesrv-node.sh
@@ -11,6 +11,7 @@ initNode() {
 }
 
 readClusterListFromNameserver() {
+  export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
   brokers="$(timeout -s SIGKILL 10s /opt/rocketmq/current/bin/mqadmin clusterList -n ${NAME_SERVER_LIST} 2>/dev/null \
               | awk 'NR > 1 { print $2,$3,$4 }' \
               | jq -R -c 'split(" ")' \
