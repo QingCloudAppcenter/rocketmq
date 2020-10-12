edb057e995b324a476d5de022784a0cdcf412493
shengpengwang <shengpengwang@yunify.com>
2020-10-12 16:12:13 +0800

    change hard code JAVA_HOME in namesrv-node.sh 2020101216

--- ansible/roles/node-namesrv/files/opt/app/current/bin/node/namesrv-node.sh
+++ ansible/roles/node-namesrv/files/opt/app/current/bin/node/namesrv-node.sh
@@ -10,7 +10,7 @@ initNode() {
 }
 
 readClusterListFromNameserver() {
-  export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
+  export JAVA_HOME
   brokers="$(timeout -s SIGKILL 10s /opt/rocketmq/current/bin/mqadmin clusterList -n ${NAME_SERVER_LIST} 2>/dev/null \
               | awk 'NR > 1 { print $2,$3,$4 }' \
               | jq -R -c 'split(" ")' \
