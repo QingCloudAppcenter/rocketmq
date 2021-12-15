c54010664e63ae0d13216db0063a390cb15ac516
mini-idea <2596691139@qq.com>
2021-12-15 17:26:14 +0800

    Merge the client node and the console node

--- ansible/roles/node-client/files/opt/app/current/bin/node/client-node.sh
+++ ansible/roles/node-client/files/opt/app/current/bin/node/client-node.sh
@@ -5,4 +5,8 @@
 initNode() {
   logger "Initializing client node..."
   echo 'ubuntu:p12cHANgepwD' | chpasswd
+  mkdir -p -m=750 /data/console
+  chown -R rocketmq:svc /data/console
+  ln -s -f /opt/app/current/conf/caddy/index.html /data/console/index.html
+  _initNode
 }
